eb3654bd451bea5454641eb884d36ca01ef252ff
Robert Ndungu Njane <robert.ndungu@hotmail.com>
2021-02-19 20:18:33 +0300

    Use mommy to simplify tests

--- FarmersApp/tests.py
+++ FarmersApp/tests.py
@@ -4,26 +4,27 @@ from rest_framework import status
 from rest_framework.authtoken.models import Token
 from django.contrib.auth.models import User
 from model_mommy import mommy
-from . import models
+from .models import Farmer
 
 
 class TestsSetUp(APITestCase):
     def setUp(self):
-        self.admin_token = Token.objects.create(user=User.objects.create_superuser(username='testadminuser', email='testadmin@test.com'))
-        self.admin_client = APIClient()
-        self.admin_client.credentials(HTTP_AUTHORIZATION='Token ' + self.admin_token.key)
-        self.test_farmer = self.admin_client.post(reverse('list_create_farmer'), {'name': 'testfarmer', 'longitude': 12.23, 'latitude': 34.55})
+        self.client = APIClient()
+        self.test_user = mommy.make(User)
+        self.token = Token.objects.create(user=self.test_user)
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+        self.test_farmer = mommy.make(Farmer, created_by=self.test_user, modified_by=self.test_user)
 
 
 class FarmersAppTests(TestsSetUp):
     '''tests related to categories'''
     def test_authenticated_user_can_add_a_farmer(self):
-        response = self.admin_client.post(reverse('list_create_farmer'), {'name': 'testfarmer', 'longitude': 12.23, 'latitude': 34.55})
+        response = self.client.post(reverse('list_create_farmer'), {'name': 'testfarmer', 'longitude': 12.23, 'latitude': 34.55})
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual('testfarmer', response.data['name'])
-    
+
     def test_authenticated_user_can_view_all_farmers(self):
-        response = self.admin_client.get(reverse('list_create_farmer'))
+        response = self.client.get(reverse('list_create_farmer'))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_farmer_creation_requires_login(self):
@@ -32,11 +33,11 @@ class FarmersAppTests(TestsSetUp):
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
     def test_authenticated_user_can_edit_a_farmer_details(self):
-        response = self.admin_client.patch(reverse('get_edit_delete_farmer', kwargs={'pk': 1}), {'name': 'new farmer name'})
+        response = self.client.patch(reverse('get_edit_delete_farmer', kwargs={'pk': 1}), {'name': 'new farmer name'})
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual('new farmer name', response.data['name'])
 
     def test_authenticated_user_can_delete_a_farmer(self):
-        response = self.admin_client.delete(reverse('get_edit_delete_farmer', kwargs={'pk': 1}))
+        response = self.client.delete(reverse('get_edit_delete_farmer', kwargs={'pk': 1}))
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
         self.assertEqual(response.data, None)
